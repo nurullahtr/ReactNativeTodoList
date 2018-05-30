59b7ba8d1ea6e8d68af9e704441b127b62319b18
Nurullah <nurullahturkyilmaz@outlook.com>
2018-05-31 01:21:07 +0300

    development continous

--- App.js
+++ App.js
@@ -10,17 +10,32 @@ import {
 } from 'react-native';
 import MyButton from './MyButton'
 
+const items = [];
+
 export default class App extends Component<{}> {
 
-  addItem(){
-    console.warn('App:addItem')
+  constructor(props){
+    super(props)
+    this.addItem = this.addItem.bind(this);
   }
 
-  renderItem(){
+  state = {
+    toDo : '',
+
+  }
+
+  addItem(){
+    
+    items.push(this.state.toDo)
+    this.setState({toDo: ''})
+
+  }
+
+  renderItem(item){
     return (
-      <View style={{ height: 100, backgroundColor: 'black', margin: 8, borderRadius: 16,justifyContent: 'center',alignItems: 'center'}} >
-        <Text style={{color: 'white', fontSize: 24}}>
-          2 Kilo patlican al :)
+      <View key={item} style={{ height: 100, backgroundColor: '#f4f4f4', margin: 8, borderRadius: 16,justifyContent: 'center',alignItems: 'center'}} >
+        <Text style={{color: 'black', fontSize: 24}}>
+          {item}
         </Text>
       </View>
     );
@@ -29,25 +44,23 @@ export default class App extends Component<{}> {
   render() {
     return (
       <View style={{flex: 1, marginTop: Platform.OS == 'ios' ? 21 : 0}}>
-        <View style={{backgroundColor: 'yellow', height: 100, flexDirection: 'row',padding: 8}}>
-          <View style={{backgroundColor: 'red', flex: 4, marginRight: 8,justifyContent: 'center'}}>
-            <TextInput placeholder={'Yapılacak işi giriniz.'} style={{height: 32,backgroundColor: '#f4f4f4', borderRadius: 12, paddingLeft:10}}>
+        <View style={{height: 100, flexDirection: 'row',padding: 8}}>
+          <View style={{flex: 4, marginRight: 8,justifyContent: 'center'}}>
+            <TextInput value={this.state.toDo} onChangeText={(x) => this.setState({ toDo : x })} placeholder={'Yapılacak işi giriniz.'} style={{height: 32,backgroundColor: '#f4f4f4', borderRadius: 12, paddingLeft:10, borderColor: 'black', borderWidth: 1}}>
             </TextInput>
           </View>
-          <View style={{backgroundColor: 'pink', flex: 1}}>
+          <View style={{flex: 1}}>
             <MyButton onPress={this.addItem} text={'EKLE'}>
             </MyButton>
           </View>
         </View>
+        <View style={{height: 0.8, backgroundColor: 'gray', marginHorizontal: 10}}>
+            
+        </View>
         <ScrollView>
-          {this.renderItem()}
-          {this.renderItem()}
-          {this.renderItem()}
-          {this.renderItem()}
-          {this.renderItem()}
-          {this.renderItem()}
-          {this.renderItem()}
-          {this.renderItem()}
+          {
+            items.map((item) => this.renderItem(item))
+          }
         </ScrollView>
       </View>
     );
