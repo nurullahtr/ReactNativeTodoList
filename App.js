03acb09772c569c4871b29b6eb75b5c769af2317
Nurullah <nurullahturkyilmaz@outlook.com>
2018-05-31 00:13:51 +0300

    development continous

--- App.js
+++ App.js
@@ -5,15 +5,42 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView
+  ScrollView,
+  TextInput
 } from 'react-native';
 
 
 export default class App extends Component<{}> {
+
+  renderItem(){
+    return (
+      <View style={{ height: 100, backgroundColor: 'black', margin: 8, borderRadius: 16,justifyContent: 'center',alignItems: 'center'}} >
+<Text style={{color: 'white', fontSize: 24}}>
+  2 Kilo patlican al :)
+</Text>
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View style={{flex: 1}}>
-        
+      <View style={{flex: 1, marginTop: Platform.OS == 'ios' ? 21 : 0}}>
+        <View style={{backgroundColor: 'yellow', height: 100, flexDirection: 'row',padding: 8}}>
+          <View style={{backgroundColor: 'red', flex: 4, marginRight: 8}}>
+          </View>
+          <View style={{backgroundColor: 'pink', flex: 1}}>
+          </View>
+        </View>
+        <ScrollView>
+          {this.renderItem()}
+          {this.renderItem()}
+          {this.renderItem()}
+          {this.renderItem()}
+          {this.renderItem()}
+          {this.renderItem()}
+          {this.renderItem()}
+          {this.renderItem()}
+        </ScrollView>
       </View>
     );
   }
